c22d23e9c0a9976eabc96d49914e1a24de687c2f
Alan Hong <xiupan@gmail.com>
2017-07-27 11:38:07 -0500

    Books#show now displays which user added the new book!

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
   belongs_to :user
   validates :title, :author, :description, :url, :user_id, presence: true
+  scope :username, -> { User.find_by(:id => Book.user_id) }
 end
